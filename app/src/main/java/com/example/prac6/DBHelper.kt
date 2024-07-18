4e4465f68b3d6a4fc845e310cfeb1ce31ecf6736
Naduni366 <158599681+Naduni366@users.noreply.github.com>
2024-07-18 17:30:54 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/prac6/DBHelper.kt
@@ -0,0 +1,18 @@
+package com.example.prac6
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper (context: Context):
+    SQLiteOpenHelper(context,"University",null,1) {
+
+    companion object{
+       const val TABLE_NAME="Student"
+        private const val SQL_CREATE_ENTRIES
+    }
+
+
+
+    }
+
